1b1294d521d4da20d39d0714603cd2d0a5ffa917
Fragiskosfarmakis <48857083+Fragiskosfarmakis@users.noreply.github.com>
2019-03-23 13:33:47 +0200

    my first commit

---
+++ main.c13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+double sqr(double x){
+	return(x * x);
+}
+voidprint_hello(void){
+	printf("Hello,world\n");
+}
+
