916951010253bb3c1e0194b7892fe3eb15b4233a
Fragiskosfarmakis <48857083+Fragiskosfarmakis@users.noreply.github.com>
2019-03-23 13:34:40 +0200

    uploaded main.12

---
+++ main.c12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int myArr[10];
+	int i=0;
+	double sum=0;
+	for(i;i<10;i++) {
+	printf("Input a number for my myArr[%d]:\n",i);
+	scanf("%d",&myArr[i]);}
+	sum+=myArr[i];
+	printf("The average is %g\n",sum\i);
+	for(i=9;i>=0;i--){
+	printf(" myArray[%d] is %d\n",myArr[i],i);
+	}
+	
+
+}
